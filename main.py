dc07f62e849275f1df71a1fa4689a9537a508872
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2019-12-20 15:53:11 +0100

    Update and rename ddns.py to main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import argparse
+import CloudFlare
+import sys
+
+if __name__== "__main__":
+  
+  parser = argparse.ArgumentParser("Dynamic DNS using Cloudflare!")
+  parser.add_argument("-e", type=str, help="CloudFlare email address", required=True,  dest='email')
+  parser.add_argument("-ak", type=str, help="CloudFlare ApiKey", required=True, dest='apikey')
+  parser.add_argument("-d", type=str, help="Domain name to be updated eg. test.com", required=True, dest='domain')
+  parser.add_argument("-r", type=str, help="Record to be updated eg. myrecord.test.com", required=True, dest='record')
+  parser.add_argument("-rt", type=str, help="Record type eg. A", required=True, dest='rtype')
+
+  try:
+    args = parser.parse_args()
+  except:
+    parser.print_help
+    sys.exit(1)
+
+  print("Starting ddns update...")
+
+  cf = CloudFlare.CloudFlare(args.email,args.apikey)
+  cf.start_dyndns(args.domain,args.record,args.rtype)
