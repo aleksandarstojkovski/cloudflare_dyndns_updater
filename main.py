d421c467d4899f6bd6b5a71e1d3892ed7d765eb2
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2019-12-28 01:01:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__== "__main__":
   parser.add_argument("-d", type=str, help="Domain name to be updated eg. test.com", required=True, dest='domain')
   parser.add_argument("-r", type=str, help="Record to be updated eg. myrecord.test.com", required=True, dest='record')
   parser.add_argument("-rt", type=str, help="Record type eg. A", required=True, dest='rtype')
+  parser.add_argument("-i", type=int, help="Check interval in seconds eg. 10", required=True, dest='interval')
 
   try:
     args = parser.parse_args()
@@ -20,4 +21,4 @@ if __name__== "__main__":
   print("Starting ddns update...")
 
   cf = CloudFlare.CloudFlare(args.email,args.apikey)
-  cf.start_dyndns(args.domain,args.record,args.rtype)
+  cf.start_dyndns(args.domain,args.record,args.rtype,args.interval)
