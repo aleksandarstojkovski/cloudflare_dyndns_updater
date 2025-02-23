3a14d3d6bd360b5d78290099a81cfca37edd531f
Aleksandar Stojkovski <43863010+aleksandarstojkovski@users.noreply.github.com>
2025-02-23 20:56:46 +0100

    Update bulk_ip_change.py

--- bulk_ip_change.py
+++ bulk_ip_change.py
@@ -1,10 +1,13 @@
 import CloudFlare
 
-cf = CloudFlare.CloudFlare("","")
 
 old_ip="178.32.51.118"
 new_ip="0.0.0.0"
 new_ttl=60
+email=""
+apikey=""
+
+cf = CloudFlare.CloudFlare(email,apikey)
 
 for zone in cf.get_zones():
     print(zone["name"])
@@ -16,7 +19,7 @@ for zone in cf.get_zones():
         record_id=record["id"]
         if (old_ip in record_content):
             new_content=record_content.replace(old_ip,new_ip)
-            #cf.update_record(zone,record_name,record_type,new_content, new_ttl)
+            cf.update_record(zone["name"],record_name,record_type,new_content, new_ttl)
             print("old -> {} {} {} {}".format(record_name,record_type,record_content, record_ttl))
             print("new -> {} {} {} {}".format(record_name,record_type,new_content, new_ttl))
 
